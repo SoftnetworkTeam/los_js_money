23ee3d00866eb13e8d567ff6ea8595fc06a80fb8
Folkie-DEV <jaturaphatpeme@gmail.com>
2025-03-07 15:11:16 +0700

    Update flot-init.js

--- static/js/plugins-init/flot-init.js
+++ static/js/plugins-init/flot-init.js
@@ -14,7 +14,7 @@
 				bars: {
 					show: true,
 					lineWidth: 0,
-					fillColor: '#7EA700'
+					fillColor: '#03A9F3'
 				}
 			},
 			grid: {
@@ -44,7 +44,7 @@
 			bars: {
 				show: true,
 				lineWidth: 0,
-				fillColor: '#7EA700'
+				fillColor: '#03A9F3'
 			}
 		}, {
 			data: [[1, 5], [3, 7], [5, 10], [7, 7], [9, 9], [11, 5], [13, 4], [15, 6]],
@@ -84,7 +84,7 @@
 			{
 				data: newCust,
 				label: 'New Customer',
-				color: '#7EA700'
+				color: '#03A9F3'
 			},
 			{
 				data: retCust,
@@ -142,7 +142,7 @@
 			{
 				data: newCust,
 				label: 'New Customer',
-				color: '#7EA700'
+				color: '#03A9F3'
 			},
 			{
 				data: retCust,
@@ -205,7 +205,7 @@
 			{
 				data: newCust2,
 				label: 'New Customer',
-				color: '#7EA700'
+				color: '#03A9F3'
 			},
 			{
 				data: retCust2,
@@ -263,7 +263,7 @@
 			{
 				data: newCust,
 				label: 'New Customer',
-				color: '#7EA700'
+				color: '#03A9F3'
 			},
 			{
 				data: retCust,
@@ -322,7 +322,7 @@
 			{
 				data: newCust,
 				label: 'New Customer',
-				color: '#7EA700'
+				color: '#03A9F3'
 			},
 			{
 				data: retCust,
@@ -446,7 +446,7 @@
 		var updateInterval = 1000;
 
 		var plot4 = $.plot('#flotRealtime1', [getRandomData()], {
-			colors: ['#7EA700'],
+			colors: ['#03A9F3'],
 			series: {
 				lines: {
 					show: true,
@@ -511,7 +511,7 @@
 		var updateInterval = 1000;
 		
 		var plot5 = $.plot('#flotRealtime2', [getRandomData()], {
-			colors: ['#7EA700'],
+			colors: ['#03A9F3'],
 			series: {
 				lines: {
 					show: true,
